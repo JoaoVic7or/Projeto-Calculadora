a05fc9f6e9da2492fd820d1bb29ae9fb2a0bbb8e
João Victor <52728391+JoaoVic7or@users.noreply.github.com>
2022-10-13 23:21:52 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function insert(num)
+{
+    var numero = document.getElementById('tela').innerHTML;
+    document.getElementById('tela').innerHTML = numero + num;
+}
+function voltar()
+{
+    var tela = document.getElementById('tela').innerHTML;
+    document.getElementById('tela').innerHTML = tela.substring(0, tela.length -1);
+}
+function limpar()
+{
+    document.getElementById('tela').innerHTML = "";
+}
+function calcular()
+{
+    var tela = document.getElementById('tela').innerHTML;
+    if(tela)
+    {
+        document.getElementById('tela').innerHTML = eval(tela);
+    }
+    else
+    {
+        document.getElementById('tela').innerHTML = "Nada..."
+    }
+}
